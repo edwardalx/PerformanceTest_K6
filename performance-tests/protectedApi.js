b4079d3770ff824e97895cb8fc87d378d09c1e95
edwardalx <eobkwaku@gmail.com>
2026-04-30 21:10:26 +0100

    diff-threshold-for-ci-updated-yml

--- performance-tests/protectedApi.js
+++ performance-tests/protectedApi.js
@@ -5,6 +5,7 @@ import { check, sleep } from "k6";
 const BASE_URL = __ENV.BASE_URL || "https://campushostels.duckdns.org";
 const EMAIL = __ENV.EMAIL;
 const PASSWORD = __ENV.PASSWORD;
+const isCI = __ENV.CI === "true";
 
 export const options = {
   stages: [
